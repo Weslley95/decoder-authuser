6ca09f52f80af7ead286fb5d4c504910fe3bf67c
Weslley Bezerra <weslleybezerra95@gmail.com>
2024-02-27 21:59:28 -0300

    feat: added controller, model, service & enum

---
+++ src/main/java/com/ead/authuser/services/impl/userServiceImpl.java
@@ -0,0 +1,33 @@
+package com.ead.authuser.services.impl;
+
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.repositories.UserRepository;
+import com.ead.authuser.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class userServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<UserModel> findAll() {
+        return this.userRepository.findAll();
+    }
+
+    @Override
+    public Optional<UserModel> findById(UUID userId) {
+        return this.userRepository.findById(userId);
+    }
+
+    @Override
+    public void delete(UserModel userModel) {
+        this.userRepository.delete(userModel);
+    }
+}
