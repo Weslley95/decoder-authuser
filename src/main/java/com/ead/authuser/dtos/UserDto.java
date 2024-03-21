a0a1b4f1953c3f51d07e9887a42da97bb4823186
Weslley Bezerra <weslleybezerra95@gmail.com>
2024-03-20 22:35:55 -0300

    feat: added validation DTO

--- src/main/java/com/ead/authuser/dtos/UserDto.java
+++ src/main/java/com/ead/authuser/dtos/UserDto.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
 import org.apache.catalina.User;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.UUID;
 
 @Data
@@ -20,15 +23,23 @@ public class UserDto {
 
     private Long userId;
 
+    @Size(min = 4, max = 50)
+    @NotBlank(groups = UserView.RegistrationPost.class)
     @JsonView(UserView.RegistrationPost.class)
     private String username;
 
+    @NotBlank
+    @Email(groups = UserView.RegistrationPost.class)
     @JsonView(UserView.RegistrationPost.class)
     private String email;
 
+    @Size(min = 6, max = 20)
+    @NotBlank(groups = {UserView.RegistrationPost.class, UserView.RegistrationPost.class})
     @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
     private String password;
 
+    @Size(min = 6, max = 20)
+    @NotBlank(groups = UserView.PasswordPut.class)
     @JsonView(UserView.PasswordPut.class)
     private String oldPassword;
 
@@ -41,6 +52,7 @@ public class UserDto {
     @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String cpf;
 
+    @NotBlank(groups = UserView.ImagePut.class)
     @JsonView(UserView.ImagePut.class)
     private String imageUrl;
 }
